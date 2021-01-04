26ef630c3d1c1f5ea9899c969f15a7bf038770bc
nitou0414 <n.itou0414@gmail.com>
2021-01-04 21:40:03 +0900

    確認依頼

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,6 +29,6 @@ class ItemsController < ApplicationController
   private
 
 def item_params
-    params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :shipping_charges_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
 end
     end
--- db/migrate/20201223073517_create_items.rb
+++ db/migrate/20201223073517_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :explanation,null:false
       t.integer :category_id,null:false
       t.integer :status_id,null:false
-      t.integer :shipping_charges_id,null:false
+      t.integer :shipping_charge_id,null:false
       t.integer :prefecture_id,null:false
       t.integer :shipping_date_id,null:false
       t.integer :price,null:false
