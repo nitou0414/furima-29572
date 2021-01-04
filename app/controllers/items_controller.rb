b812a15e46f5baae192813efe42c566dbf2f3a91
nitou0414 <n.itou0414@gmail.com>
2021-01-04 13:47:54 +0900

    確認依頼

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :show]
-  before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.all.order('id DESC')
+    #@items = Item.all.order('id DESC')
   end
 
   def new 
@@ -19,8 +18,8 @@ class ItemsController < ApplicationController
     end
   end
 
-def edit
-end
+# def edit
+# end
 
 # def update
 #   @item.update(item_params)
@@ -32,10 +31,4 @@ end
 def item_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :shipping_charges_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
 end
-
-def move_to_index
-      unless user_signed_in?
-        redirect_to action: :index
     end
-    end
-  end
