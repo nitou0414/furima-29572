cdb60fa69d915285a9fbefa45a49182d02044dfb
nitou0414 <n.itou0414@gmail.com>
2021-01-04 12:12:44 +0900

    確認依頼

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!, only: [:new, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order('id DESC')
@@ -28,7 +29,13 @@ end
 
   private
 
-  def item_params
+def item_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :shipping_charges_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
 end
+
+def move_to_index
+      unless user_signed_in?
+        redirect_to action: :index
+    end
+    end
+  end
