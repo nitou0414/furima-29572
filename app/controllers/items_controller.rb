ca0975bec1ee1a45deac8d960b0e0b2804776ac6
nitou0414 <n.itou0414@gmail.com>
2021-01-05 22:48:54 +0900

    確認依頼

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :show]
+  before_action :authenticate_user!, only: [new]
+  before_action :set_item, only: [:edit, :show]
+
 
   def index
     @items = Item.all.order('id DESC')
@@ -10,7 +12,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item=Item.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
     redirect_to root_path
     else
@@ -18,17 +20,25 @@ class ItemsController < ApplicationController
     end
   end
 
-# def edit
-# end
+ def edit
+ end
 
-# def update
-#   @item.update(item_params)
-#   redirect_to root_path
-# end
+#  def show
+#  end
+
+ def update
+   @item.update(item_params)
+   redirect_to root_path
+ end
+
+ def set_item
+  @item = Item.find(params[:id])
+end
+
 
   private
 
 def item_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
 end
-    end
+end
