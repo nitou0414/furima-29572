25454b06e1659b99fcd8ab672305ca409494c635
nitou0414 <n.itou0414@gmail.com>
2021-01-07 15:34:21 +0900

    確認依頼

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,6 @@ class ItemsController < ApplicationController
   #set_〇〇=まとめる
   before_action :set_item, only: [:edit, :show, :update]
 
-
   def index
     @items = Item.all.order('id DESC')
   end
@@ -16,16 +15,25 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-    redirect_to root_path
+      redirect_to root_path
     else
       render :new
     end
   end
 
   def edit
-    if current_user.id ！= @item.user_id 
+    if current_user.id != @item.user_id 
       redirect_to root_path
+    end
   end
+
+  def update
+    if current_user.id == @item.user_id 
+      @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def show
@@ -37,18 +45,13 @@ class ItemsController < ApplicationController
   #   redirect_to root_path
   end
 
- def update
-  @item.update(item_params)
-   redirect_to root_path
- end
-
- def set_item
-  @item = Item.find(params[:id])
-end
-
   private
 
-def item_params
-    params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
-end
+  def item_params
+      params.require(:item).permit(:name, :image, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
