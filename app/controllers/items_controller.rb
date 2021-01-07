856e65f3fc2071df945c207fc531c31660f4ebbe
nitou0414 <n.itou0414@gmail.com>
2021-01-07 12:43:38 +0900

    確認依頼

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  #authenticate_user!=ログインしていなかったら
+  before_action :authenticate_user!, only: [:new, :edit]
+  #set_〇〇=まとめる
   before_action :set_item, only: [:edit, :show, :update]
 
 
@@ -27,9 +29,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-   @items = Item.find(params[:id])
-    @items.destroy
-    redirect_to root_path
+  #  @items = Item.find(params[:id])
+  #   @items.destroy
+  #   redirect_to root_path
   end
 
  def update
