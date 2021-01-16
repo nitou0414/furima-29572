cb701b79b5f0f520b26a043a5f47aaaddc805c62
nitou0414 <n.itou0414@gmail.com>
2021-01-16 16:15:00 +0900

    デプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :authenticate_user!, except: [:index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password,:family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+  end
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
   end
 end
